d09d74c4b699b818d21296f6da5b16216552fd18
主計義高 <ness.kazuyoshi@gmail.com>
2017-12-21 23:27:41 +0900

    add Embedded Frameworks

---
+++ eventry/Domain/Domain.h
@@ -0,0 +1,18 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by 主計義高 on 2017/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Domain.
+FOUNDATION_EXPORT double DomainVersionNumber;
+
+//! Project version string for Domain.
+FOUNDATION_EXPORT const unsigned char DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Domain/PublicHeader.h>
+
+
