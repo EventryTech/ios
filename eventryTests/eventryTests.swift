35344adbc72d9f569dcd36d6d2d8143b635c1071
主計義高 <ness.kazuyoshi@gmail.com>
2017-12-21 21:57:47 +0900

    Initial Commit

---
+++ eventryTests/eventryTests.swift
@@ -0,0 +1,36 @@
+//
+//  eventryTests.swift
+//  eventryTests
+//
+//  Created by 主計義高 on 2017/12/21.
+//  Copyright © 2017年 主計義高. All rights reserved.
+//
+
+import XCTest
+@testable import eventry
+
+class eventryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
